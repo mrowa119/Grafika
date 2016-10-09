f9a594a8ac8000db7bcd8eec9eb890edf99936a6
mrowa119 <mrowa119@gmail.com>
2016-10-09 21:46:19 +0200

    [Lab1][Zad1c] chessboard and 45 degree rotation

---
+++ src/Lab1/Exercise1ca.java
@@ -0,0 +1,31 @@
+package Lab1;
+
+public class Exercise1ca extends MyImage implements Processable {
+
+	// Kolory
+	final int figureColor = Tools.int2RGB(0, 0, 0);
+	final int backgroundColor = Tools.int2RGB(255, 255, 255);
+
+	// Rozmial pola
+	final int fieldSize = 100;
+
+	@Override
+	public void processImage(int i, int j) {
+		int distanceFromSideCenterElementX = Math.abs(xC - j) + fieldSize / 2;
+		int distanceFromSideCenterElementY = Math.abs(yC - i) + fieldSize / 2;
+
+		int horisontalElementDiffCount = distanceFromSideCenterElementX / fieldSize;
+		int verticalElementDiffCount = distanceFromSideCenterElementY / fieldSize;
+
+		if ((horisontalElementDiffCount + verticalElementDiffCount) % 2 == 0) {
+			setColour(i, j, figureColor);
+		} else {
+			setColour(i, j, backgroundColor);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Exercise1ca().renderAndSave();
+	}
+
+}
---
+++ src/Lab1/Exercise1cb.java
@@ -0,0 +1,37 @@
+package Lab1;
+
+public class Exercise1cb extends MyImage implements Processable {
+
+	// Kolory
+	final int figureColor = Tools.int2RGB(0, 0, 0);
+	final int backgroundColor = Tools.int2RGB(255, 255, 255);
+
+	// Rozmial pola
+//	final int fieldSize = (int)(500/Math.sqrt(2));
+	final int fieldSize = 20;
+
+	// Po³owa przek¹tnej
+	final int diagonalOfSquare = (int) (fieldSize * Math.sqrt(2));
+	final int halfDiagonalOfSquare = diagonalOfSquare / 2;
+
+	@Override
+	public void processImage(int i, int j) {
+		int distanceFromSideCenterX = Math.abs(xC - j);
+		int distanceFromSideCenterY = Math.abs(yC - i);
+
+		int distanceFromCenterOfVirtualSquareX = Math.abs((distanceFromSideCenterX%diagonalOfSquare)-halfDiagonalOfSquare);
+		int distanceFromCenterOfVirtualSquareY = Math.abs((distanceFromSideCenterY%diagonalOfSquare)-halfDiagonalOfSquare);
+		
+
+		if ((distanceFromCenterOfVirtualSquareX+distanceFromCenterOfVirtualSquareY)<halfDiagonalOfSquare){
+			setColour(i, j, figureColor);
+		} else {
+			setColour(i, j, backgroundColor);
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Exercise1cb().renderAndSave();
+	}
+
+}

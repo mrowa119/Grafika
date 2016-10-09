216806bbce65dd5085d733a9e1c8e1b02282785e
mrowa119 <mrowa119@gmail.com>
2016-10-09 22:33:38 +0200

    [Lab1][Exe2b]

---
+++ src/Lab1/Exercise2b.java
@@ -0,0 +1,45 @@
+package Lab1;
+
+public class Exercise2b extends LoadedImage {
+
+	// Szerokoœæ lini kratki
+	final int lineWidth = 50;
+
+	// Odleg³oœæ pomiêdzy œrodkami lini
+	final int distanceBetweenMiddleOfLinesX = 3 * lineWidth;
+	final int distanceBetweenMiddleOfLinesY = 2 * lineWidth;
+
+	// Kolory
+	final int figureColor = Tools.int2RGB(0, 0, 0);
+
+	@Override
+	public void processImage(int i, int j) {
+		int distanceFromCenterX = Math.abs(xC - j);
+		int distanceFromCenterY = Math.abs(yC - i);
+
+		if (distanceFromApsoluteCenterX(distanceFromCenterX) > distanceFromMiddleLine()
+				&& distanceFromApsoluteCenterY(distanceFromCenterY) > distanceFromMiddleLine()) {
+			setColour(i, j, getColour(i, j));
+		} else {
+			setColour(i, j, figureColor);
+		}
+
+	}
+
+	private int distanceFromMiddleLine() {
+		return lineWidth / 2;
+	}
+
+	private int distanceFromApsoluteCenterY(int distanceFromCenterY) {
+		return Math.abs(distanceFromCenterY % distanceBetweenMiddleOfLinesY - (distanceBetweenMiddleOfLinesY / 2));
+	}
+
+	private int distanceFromApsoluteCenterX(int distanceFromCenterX) {
+		return Math.abs(distanceFromCenterX % distanceBetweenMiddleOfLinesX - (distanceBetweenMiddleOfLinesX / 2));
+	}
+
+	public static void main(String[] args) {
+		new Exercise2b().renderAndSave();
+	}
+
+}

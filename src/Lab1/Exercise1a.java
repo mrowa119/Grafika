4232d3e753a89b04449c465e65202851bd440625
mrowa119 <mrowa119@gmail.com>
2016-10-12 19:41:31 +0200

    [Lab1][Exe1a] Add secound gradiend

--- src/Lab1/Exercise1a.java
+++ src/Lab1/Exercise1a.java
@@ -4,10 +4,11 @@ public class Exercise1a extends MyImage implements Processable {
 
 	// Kolory
 	int figureColor = Tools.int2RGB(0, 0, 0);
+	int figure2Color = Tools.int2RGB(0, 255, 0);
 	int backgroundColor = Tools.int2RGB(255, 255, 255);
 
 	// Szerokosc pierscienia
-	final int ringWidth = 10;
+	final int ringWidth = 20;
 	final int gradientWidth = 60;
 
 	@Override
@@ -21,16 +22,21 @@ public class Exercise1a extends MyImage implements Processable {
 
 
 	private int getRingColour(double radius) {
-		int apsoluteRadius = (int) (radius % (2 * ringWidth + gradientWidth));
-		if (isFigure(apsoluteRadius)) {
+		int absoluteRadius = (int) (radius % ((2 * ringWidth + 2 * gradientWidth)));
+		if (isFigure(absoluteRadius)) {
 			return figureColor;
-		} else if (isBackround(apsoluteRadius)) {
+		} else if (isFirstGradiend(absoluteRadius)){
+			int coursorPositionInGradient = absoluteRadius - ringWidth;
+			return getGradientFieldColour(gradientWidth-coursorPositionInGradient);
+		} else if (isBackround(absoluteRadius)) {
 			return backgroundColor;
 		} else {
-			int coursorPositionInGradient = apsoluteRadius - 2 * ringWidth;
+			int coursorPositionInGradient = absoluteRadius - 2 * ringWidth - gradientWidth;
 			return getGradientFieldColour(coursorPositionInGradient);
 		}
 	}
+
+
 
 	private int getGradientFieldColour(int coursorPositionInGradient) {
 		int figureRed = Tools.RGB2red(figureColor);
@@ -49,13 +55,17 @@ public class Exercise1a extends MyImage implements Processable {
 
 		return Tools.int2RGB(gradientRed, gradientGreen, gradientBlue);
 	}
-
-	private boolean isBackround(int apsoluteRadius) {
-		return apsoluteRadius < 2 * ringWidth;
+	
+	private boolean isFirstGradiend(int absoluteRadius) {
+		return absoluteRadius < (ringWidth + gradientWidth);
 	}
 
-	private boolean isFigure(int apsoluteRadius) {
-		return apsoluteRadius < ringWidth;
+	private boolean isBackround(int absoluteRadius) {
+		return absoluteRadius < (2 * ringWidth + gradientWidth);
+	}
+
+	private boolean isFigure(int absoluteRadius) {
+		return absoluteRadius < ringWidth;
 	}
 
 	public static void main(String[] args) {

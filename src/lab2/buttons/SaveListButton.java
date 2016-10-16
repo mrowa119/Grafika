140800bdf3e4b4f0224255fdfec9d8e715784277
mrowa119 <mrowa119@gmail.com>
2016-10-16 22:26:22 +0200

    [Lab2] Typos

--- src/lab2/buttons/SaveListButton.java
+++ src/lab2/buttons/SaveListButton.java
@@ -7,6 +7,6 @@ public class SaveListButton extends JButton {
 	private static final long serialVersionUID = 8909873722727362788L;
 
 	public SaveListButton() {
-		super("Zapisz listês");
+		super("Zapisz listê");
 	}
 }

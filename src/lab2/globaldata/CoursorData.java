e1762f377fb9b4c1ab4304ab12ac04be07a146a6
mrowa119 <mrowa119@gmail.com>
2016-10-16 22:55:50 +0200

    [Lab1]RadioButtons listners choise to variable

---
+++ src/lab2/globaldata/CoursorData.java
@@ -0,0 +1,10 @@
+package lab2.globaldata;
+
+public class CoursorData {
+	public static int RED_COURSOR;
+	public static int GREEN_COURSOR;
+	public static int BLUE_COURSOR;
+	
+	public static int SHAPE_COURSOR;
+	
+}
--- src/lab2/panels/ShapeChooserPanel.java
+++ src/lab2/panels/ShapeChooserPanel.java
@@ -1,6 +1,8 @@
 package lab2.panels;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -8,17 +10,42 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.Border;
 
+import lab2.globaldata.CoursorData;
+
 public class ShapeChooserPanel extends JPanel {
 
 	private static final long serialVersionUID = -837412013778194102L;
 
 	public ShapeChooserPanel() {
 		setLayout(new GridLayout(3, 1));
-		JRadioButton rectangle = new JRadioButton("Prostok¹t");
+		JRadioButton rectangle = new JRadioButton("Prostok¹t", true);
 		JRadioButton ellipse = new JRadioButton("Elipsa");
 		JRadioButton polygon = new JRadioButton("Wielok¹t");
 
+		rectangle.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CoursorData.SHAPE_COURSOR = 0;
+			}
+		});
+		ellipse.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CoursorData.SHAPE_COURSOR = 1;
+			}
+		});
+		polygon.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CoursorData.SHAPE_COURSOR = 2;
+			}
+		});
+
 		ButtonGroup shapesGroup = new ButtonGroup();
+		
 		shapesGroup.add(rectangle);
 		shapesGroup.add(ellipse);
 		shapesGroup.add(polygon);

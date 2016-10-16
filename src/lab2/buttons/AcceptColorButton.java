d18045a54ff523616a763b5a3c87d9ab4f550f30
mrowa119 <mrowa119@gmail.com>
2016-10-16 23:13:31 +0200

    [Lab2] color to variable

---
+++ src/lab2/buttons/AcceptColorButton.java
@@ -0,0 +1,36 @@
+package lab2.buttons;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JSpinner;
+
+import lab2.globaldata.CoursorData;
+
+public class AcceptColorButton extends JButton {
+
+	private static final long serialVersionUID = 7690089614652881027L;
+
+	private JSpinner redSpinner;
+	private JSpinner greenSpinner;
+	private JSpinner blueSpinner;
+
+	public AcceptColorButton(JSpinner redSpinner, JSpinner greenSpinner, JSpinner blueSpinner) {
+		super("Akceptuj kolor");
+		this.redSpinner = redSpinner;
+		this.greenSpinner = greenSpinner;
+		this.blueSpinner = blueSpinner;
+
+		addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CoursorData.RED_COURSOR = (int) redSpinner.getValue();
+				CoursorData.GREEN_COURSOR = (int) greenSpinner.getValue();
+				CoursorData.BLUE_COURSOR = (int) blueSpinner.getValue();
+			}
+		});
+	}
+
+}

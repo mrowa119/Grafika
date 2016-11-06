1356ef9bb54af46c262c0419addc4a1e35472a14
mrowa119 <mrowa119@gmail.com>
2016-11-06 18:56:00 +0100

    [Lab3][Exe4] Draw Axis

--- src/lab3/Exercise1/Cartesian.java
+++ src/lab3/Exercise1/Cartesian.java
@@ -1,15 +1,68 @@
 package lab3.Exercise1;
 
+import java.awt.Color;
+import java.awt.Polygon;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Cartesian extends BufferedImage {
-	
+
+	private final String outputFileName = "Cartesian.bmp";
+	private final String outputFileFormat = "bmp";
+
 	public Cartesian(int width, int height) {
 		super(width, height, BufferedImage.TYPE_INT_RGB);
+		setBacground(Color.white);
+		createAxisX();
+		createAxisY();
 	}
 
+	private void createAxisX() {
+		int middleX = getHeight() / 2;
+		for (int i = 0; i < getWidth(); i++) {
+			setColour(i, middleX, Color.BLACK);
+		}
+	}
 	
-	public static int getDimmension(int min, int max){
-		return Math.abs(min)+Math.abs(max)+1;
+	private void createAxisY() {
+		int middleY = getWidth() / 2;
+		for (int i = 0; i < getHeight(); i++) {
+			setColour(middleY, i, Color.BLACK);
+		}
+	}
+
+	private void setBacground(Color color) {
+		for (int i = 0; i < getWidth(); i++) {
+			for (int j = 0; j < getHeight(); j++) {
+				setColour(i, j, color);
+			}
+		}
+	}
+
+	private void setColour(int width, int height, Color color) {
+		setRGB(width, height, color.getRGB());
+	}
+
+	protected void saveToFile() {
+		try {
+			File outputFile = new File(outputFileName);
+			ImageIO.write(this, outputFileFormat, outputFile);
+			System.out.println("Succes. Image stored in " + outputFile.getAbsolutePath());
+		} catch (IOException e) {
+			System.out.println("Image cannot be stored");
+		}
+	}
+
+	public static int getDimmension(int min, int max) {
+		return 2 * Math.max(Math.abs(min), Math.abs(max)) + 1;
+	}
+
+	public void drawPolygon(Polygon[] polygons) {
+		for(int i=0; i<polygons.length; i++){
+			
+		}
 	}
 }
--- src/lab3/Exercise1/Main.java
+++ src/lab3/Exercise1/Main.java
@@ -1,16 +1,37 @@
 package lab3.Exercise1;
 
+import java.awt.Polygon;
+
 public class Main {
 	
 	static int TEMP_MIN_X = -50;
-	static int TEMP_MAX_X = 50;
-	static int TEMP_MIN_Y = -50;
+	static int TEMP_MAX_X = 75;
+	static int TEMP_MIN_Y = -100;
 	static int TEMP_MAX_Y = 50;
+	
+	static Polygon[] polygons; 
+
 
 	public static void main(String[] args) {
 		int width = Cartesian.getDimmension(TEMP_MIN_X, TEMP_MAX_X);
 		int height = Cartesian.getDimmension(TEMP_MIN_Y, TEMP_MAX_Y);
+		
+		polygons = new Polygon[2];
+		
+		polygons[0] = new Polygon();
+		polygons[0].addPoint(10, 10);
+		polygons[0].addPoint(20, 20);
+		polygons[0].addPoint(-30, -30);
+		
+		polygons[1] = new Polygon();
+		polygons[1].addPoint(10, 10);
+		polygons[1].addPoint(20, 10);
+		
+		
 		Cartesian cartesian = new Cartesian(width, height);
+		cartesian.drawPolygosn(polygons);
+		cartesian.saveToFile();
 	}
+
 
 }
